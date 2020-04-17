47655451b976a4cfc3157b2b3e2bb46567677f56
newnakashima <newnakashima2014@gmail.com>
2020-04-18 08:55:39 +0900

    meta

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -70,6 +70,7 @@ const BlogPost = ({ data }) => {
               name="description"
               content={`${post.frontmatter.description}`}
             />
+            <meta property="og:description" content={`${post.frontmatter.title}`} />
           </Helmet>
         }
         tags={post.frontmatter.tags}
