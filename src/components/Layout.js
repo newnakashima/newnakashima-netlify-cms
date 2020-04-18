d0a0736c634c6e8001ce0f4a3c6559f83c730820
newnakashima <newnakashima2014@gmail.com>
2020-04-18 10:45:27 +0900

    image

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -38,7 +38,7 @@ const TemplateWrapper = ({ children }) => {
           href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
           color="#ff4400"
         />
-        <meta name="theme-color" content="#fff" />
+        <meta name="theme-color" content="#88f" />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@InNksm" />
         <meta name="twitter:creator" content="@InNksm" />
